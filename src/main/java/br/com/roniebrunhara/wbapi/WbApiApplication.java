b84fa665e41e5c94305333c230fd390be1e844ba
roniebrunhara <roniebrunhara@gmail.com>
2023-02-20 09:31:37 -0300

    primeiro commit

---
+++ src/main/java/br/com/roniebrunhara/wbapi/WbApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.roniebrunhara.wbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/roniebrunhara/wbapi/WbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.roniebrunhara.wbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
